51f7c9f397d69db5471514a11be5e2b3e7a78513
jenn-h99 <whsie041@uottawa.ca>
2023-06-14 15:38:39 -0400

    update core.py

--- behavior-experiments/operant_prob2_opto_explt.py
+++ behavior-experiments/operant_prob2_opto_explt.py
@@ -255,7 +255,7 @@ for trial in trials:
                 if np.random.rand() < rule.p_rew:
                     data.t_rew_r[trial] = (time.time() * 1000
                                            - data._t_start_abs[trial])
-                    time.sleep(0.7)
+                    time.sleep(1)
                     water_R.Reward()
                     data.v_rew_r[trial] = reward_size
 
@@ -280,7 +280,7 @@ for trial in trials:
                 else:
                     data.t_rew_l[trial] = (time.time() * 1000
                                            - data._t_start_abs[trial])
-                    time.sleep(0.7)
+                    time.sleep(1)
                     water_L.Reward()
                     data.v_rew_l[trial] = reward_size
 
