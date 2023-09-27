e2a29696de20016038cae76f7352173ad3295f57
jenn-h99 <whsie041@uottawa.ca>
2023-09-27 13:57:29 -0400

    hello

--- behavior-experiments/core.py
+++ behavior-experiments/core.py
@@ -467,12 +467,12 @@ class data():
                   f'behavior-experiments/{self.filename} '
                   f'/home/pi/Desktop/yesterday_data')
         # Create remote folder for today's data and copy file into that folder
-        os.system(f'rclone mkdir data1:"Behaviour data/Jennifer/'
+        os.system(f'rclone mkdir data1:"/Behaviour data/Jennifer/'
                   f'all mice/{self.mouse_number}"')
-        os.system(f'rclone mkdir data1:"Behaviour data/Jennifer/'
+        os.system(f'rclone mkdir data1:"/Behaviour data/Jennifer/'
                   f'all mice/{self.mouse_number}/{self.date_experiment}"')
         os.system(f'rclone copy /home/pi/Desktop/yesterday_data/{self.filename}'
-                  f' data1:"Behaviour data/Jennifer/all mice/'
+                  f' data1:"/Behaviour data/Jennifer/all mice/'
                   f'{self.mouse_number}/{self.date_experiment}"')
 
 
