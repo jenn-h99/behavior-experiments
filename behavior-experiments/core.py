c2eb692a05d1165a3d2336a91853813e62b7b40d
jenn-h99 <whsie041@uottawa.ca>
2023-10-08 11:31:16 -0400

    hello

--- behavior-experiments/core.py
+++ behavior-experiments/core.py
@@ -552,7 +552,7 @@ class stepper():
 
         print('the syringe is empty')
 
-        self.Motor(0, 30000)  # Pull the syringe for 60000 steps, ~3mL.
+        self.Motor(0, 50000)  # Pull the syringe for 60000 steps, ~3mL.
 
     def Disable(self):
 
@@ -582,7 +582,7 @@ class stepper():
         '''
         Fills the syringe pump.
         '''
-        self.Motor(0, 60000)  # Pull the syringe for 60000 steps, ~3mL.
+        self.Motor(0, 50000)  # Pull the syringe for 60000 steps, ~3mL.
 
 
 class lickometer():
