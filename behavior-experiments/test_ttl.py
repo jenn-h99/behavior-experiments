2d44d502901d0be04f3f422d2b7d08bb693c47e7
jenn-h99 <110114367+jenn-h99@users.noreply.github.com>
2023-12-11 14:17:37 -0500

    Add files via upload

---
+++ behavior-experiments/test_ttl.py
@@ -0,0 +1,31 @@
+import time
+import RPi.GPIO as GPIO
+
+# SETUP GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# DEFINE PINS
+ttl_trigger_PIN = 15
+ttl_marker_PIN = 27
+
+# PULSE LENGTH
+pulse_length = 250
+
+# INITIALIZE PINS
+GPIO.setup(ttl_trigger_PIN, GPIO.OUT)
+GPIO.output(ttl_trigger_PIN, False)
+GPIO.setup(ttl_maker_PIN, GPIO.OUT)
+GPIO.output(ttl_marker_PIN, False)
+
+#SCRIPT
+loopstat = 1
+while loopstat > 0:
+  GPIO.output(ttl_trigger_PIN, True)
+  GPIO.output(ttl_marker_PIN, True)
+  print("both on")
+  time.sleep(pulse_length)
+  GPIO.output(ttl_trigger_PIN, False)
+  GPIO.output(ttl_marker_PIN, False)
+  print("both off")
+  time.sleep(pulse_length)
