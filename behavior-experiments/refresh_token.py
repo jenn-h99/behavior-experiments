d37b6a2d6a26b54fba7fa6fdaf43ec03f4ed2550
jenn-h99 <110114367+jenn-h99@users.noreply.github.com>
2025-08-25 13:22:20 -0400

    Update refresh_token.py

--- behavior-experiments/refresh_token.py
+++ behavior-experiments/refresh_token.py
@@ -1,4 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Get new token from user
 token = input("Enter new Box token: ")
-with open('box_config.py', 'w') as f:
-    f.write(f'BOX_ACCESS_TOKEN = "{token}"')
-print("Token updated!")
+
+# Write to config file using older Python syntax
+try:
+    with open('box_config.py', 'w') as f:
+        f.write('BOX_ACCESS_TOKEN = "{}"\n'.format(token))
+    print("Token updated successfully!")
+except IOError as e:
+    print("Error writing config file: {}".format(e))
+except Exception as e:
+    print("Unexpected error: {}".format(e))
+
+# Verify the file was created
+try:
+    with open('box_config.py', 'r') as f:
+        content = f.read()
+        print("Config file contents:")
+        print(content)
+except IOError:
+    print("Could not read the config file")
+    
