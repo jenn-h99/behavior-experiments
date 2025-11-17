5416b23d09a1563a5d794be267d63c68f5f353fc
jenn-h99 <110114367+jenn-h99@users.noreply.github.com>
2025-11-17 09:51:39 -0500

    Update get_box_tokens.py

--- behavior-experiments/get_box_tokens.py
+++ behavior-experiments/get_box_tokens.py
@@ -25,5 +25,5 @@ def get_tokens(auth_code):
         print(f"Response: {response.text}")
 
 if __name__ == "__main__":
-    auth_code = 'UHk9NRsgTGQHRJDOyuNhejPz9IEtf0B2'
+    auth_code = 'ur3cNHL1Wc0BDRZNNj34dO4egQfrbXDj'
     get_tokens(auth_code)
